f6e3ec74a782037ee86c3af0b36b84ef3aea4237
Vitaliy <reamonn2008@mail.ru>
2023-07-31 02:11:17 +0300

    added link in project

--- README.md
+++ README.md
@@ -45,4 +45,4 @@ You can learn more in the [Create React App documentation](https://facebook.gith
 
 To learn React, check out the [React documentation](https://reactjs.org/).
 
-
+React burger [cсылка на проект](https://vetalb91.github.io/react/#/)
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/app/app";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter,HashRouter  } from "react-router-dom";
 import store from "./services/store";
 
 
@@ -12,10 +12,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter >
             <Provider store={store}>
                 <App />
             </Provider>
-        </BrowserRouter>
+        </HashRouter >
     </React.StrictMode>
 );
